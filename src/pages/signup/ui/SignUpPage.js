e8e80ca27390cb66dea00bb6b12a10a71559b631
happy7yong <happy7yong83@gmail.com>
2025-11-08 11:23:15 +0900

    refactory : 공통 css로 묶음

--- src/pages/signup/ui/SignUpPage.js
+++ src/pages/signup/ui/SignUpPage.js
@@ -1,30 +1,12 @@
 import "../../../shared/ui/InputField/InputField.js";
-import Button from "../../../shared/ui/Button/Button.js";
+import Button from "../../../shared/ui/Button/ui/Button.js";
 import LoginButton from "../../../features/navigation/LoginButton.js";
 
 export default function SignUpPage() {
   const signupPage = document.createElement("div");
   signupPage.className = "signup-page";
   signupPage.innerHTML = /*HTML*/ `
-      <style>
-            .signup-container{
-                display:flex;
-                flex-direction:column;
-
-                align-items:center;
-                justify-content:center;
-
-                height:100vh;
-
-                background-color:var(--color-background);
-
-                    max-width: 400px;
-                    margin: 0 auto;
-                    gap: 20px;
-            }
-
-      </style>
-      <div class="signup-container">
+      <div class="auth-container">
         <h2>회원가입</h2>
         <input-field type="text" placeholder="이메일을 입력하세요" helperText="*helper Text">이메일*</input-field>
         <input-field type="password" placeholder="비밀번호를 입력하세요" helperText="*비밀번호를 확인해주세요">비밀번호*</input-field>
@@ -36,7 +18,7 @@ export default function SignUpPage() {
   `;
   const loginButton = LoginButton();
   const primaryButton = Button({ text: "회원가입" });
-  const container = signupPage.querySelector(".signup-container");
+  const container = signupPage.querySelector(".auth-container");
   container.appendChild(primaryButton);
   container.appendChild(loginButton);
 
