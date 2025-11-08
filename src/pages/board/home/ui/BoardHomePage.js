fd204c179ae3f3041f6c8f0f3cf55f0b7ae57a0b
happy7yong <happy7yong83@gmail.com>
2025-11-09 03:10:37 +0900

    feat : board page 생성

---
+++ src/pages/board/home/ui/BoardHomePage.js
@@ -0,0 +1,12 @@
+function BoardHomePage() {
+  const boardHomePage = document.createElement("div");
+  boardHomePage.className = "board-home-page";
+
+  boardHomePage.innerHTML = /*HTML*/ `<div>
+    안녕하세요
+  </div>`;
+
+  return boardHomePage;
+}
+
+export default BoardHomePage;
