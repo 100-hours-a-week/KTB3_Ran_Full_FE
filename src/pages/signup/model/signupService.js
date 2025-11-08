4fd5aad95766489b2f5a61187a6244ed1df7eef9
happy7yong <happy7yong83@gmail.com>
2025-11-09 01:51:34 +0900

    feat : 유효성 검사 = blur로 처리

--- src/pages/signup/model/signupService.js
+++ src/pages/signup/model/signupService.js
@@ -1,8 +1,4 @@
 import signup from "../../../features/auth/api/signup.js";
-import validateConfirmPassword from "../../../features/auth/lib/confirmPassword.js";
-import validateEmail from "../../../features/auth/lib/validateEmail.js";
-import validatePassword from "../../../features/auth/lib/validatePassword.js";
-import validateUsername from "../../../features/auth/lib/validateUsername.js";
 
 async function SignupService({
   emailField,
@@ -26,38 +22,18 @@ async function SignupService({
     password,
     confirmPassword,
   };
+  try {
+    const signupAPI = await signup(signupProps);
+    console.log("signupAPI", signupAPI);
+  } catch (error) {
+    //중복된 이메일일 경우
+    const message = `*${error.message}` || "*회원가입이 불가합니다.";
 
-  const emailError = validateEmail(email);
-  const passwordError = validatePassword(password);
-  const passwordConfirmError = validateConfirmPassword(passwordConfirmProps);
-  const usernameFieldError = validateUsername(username);
+    //백엔드에서 필드를 함께 넘겨줘야하지만 백엔드 코드를 수정을 일시적으로 미루기 위해 임의로 작성해두었습니다...
+    if (message.includes("닉네임")) usernameField.helperText = message;
+    if (message.includes("이메일")) emailField.helperText = message;
 
-  //유효성 검사
-  emailField.helperText = emailError;
-  passwordField.helperText = passwordError;
-  passwordConfirmField.helperText = passwordConfirmError;
-  usernameField.helperText = usernameFieldError;
-
-  //유효성 통과
-  if (
-    !emailError &&
-    !passwordError &&
-    !passwordConfirmError &&
-    !usernameFieldError
-  ) {
-    try {
-      const signupAPI = await signup(signupProps);
-      console.log("signupAPI", signupAPI);
-    } catch (error) {
-      //중복된 이메일일 경우
-      const message = `*${error.message}` || "*회원가입이 불가합니다.";
-
-      //백엔드에서 필드를 함께 넘겨줘야하지만 백엔드 코드를 수정을 일시적으로 미루기 위해 임의로 작성해두었습니다...
-      if (message.includes("닉네임")) usernameField.helperText = message;
-      if (message.includes("이메일")) emailField.helperText = message;
-
-      //중복된 닉네임일 경우
-    }
+    //중복된 닉네임일 경우
   }
 }
 
--- src/pages/signup/ui/SignUpPage.js
+++ src/pages/signup/ui/SignUpPage.js
@@ -1,6 +1,10 @@
 import "../../../shared/ui/InputField/InputField.js";
 import { signupBtn } from "../../../shared/ui/Button/ui/ButtonPresets.js";
 import LoginButton from "../../../features/navigation/LoginButton.js";
+import validateConfirmPassword from "../../../features/auth/lib/confirmPassword.js";
+import validateEmail from "../../../features/auth/lib/validateEmail.js";
+import validatePassword from "../../../features/auth/lib/validatePassword.js";
+import validateUsername from "../../../features/auth/lib/validateUsername.js";
 
 export default function SignUpPage() {
   const signupPage = document.createElement("div");
@@ -12,7 +16,6 @@ export default function SignUpPage() {
         <input-field id="password" type="password" placeholder="비밀번호를 입력하세요">비밀번호*</input-field>
         <input-field id="passwordConfirm" type="password" placeholder="비밀번호를 한번 더 입력하세요">비밀번호 확인*</input-field>
         <input-field id="username" type="text" placeholder="닉네임을 입력하세요">닉네임*</input-field>
-        
         </div>
 
   `;
@@ -23,5 +26,69 @@ export default function SignUpPage() {
   container.appendChild(signupButton);
   container.appendChild(loginNavButton);
 
+  //포커싱 blur 이벤트 사용
+  const emailField = container.querySelector("#email");
+  const passwordField = container.querySelector("#password");
+  const passwordConfirmField = container.querySelector("#passwordConfirm");
+  const usernameField = container.querySelector("#username");
+
+  const button = container.querySelector("button");
+
+  function __updateState() {
+    const email = emailField.value;
+    const password = passwordField.value;
+    const confirmPassword = passwordConfirmField.value;
+    const username = usernameField.value;
+
+    const passwordConfirmProps = {
+      password,
+      confirmPassword,
+    };
+
+    if (
+      !validateEmail(email) &&
+      !validatePassword(password) &&
+      !validateConfirmPassword(passwordConfirmProps) &&
+      !validateUsername(username)
+    ) {
+      button.disabled = false;
+      button.classList.remove("disabled");
+    } else {
+      button.disabled = true;
+      button.classList.add("disabled");
+    }
+  }
+
+  //shadow DOM을 왜 도입했을까..~
+  emailField.addEventListener("field-blur", (e) => {
+    const email = e.detail.value;
+    emailField.helperText = validateEmail(email);
+    __updateState();
+  });
+
+  passwordField.addEventListener("field-blur", (e) => {
+    const password = e.detail.value;
+    passwordField.helperText = validatePassword(password);
+    __updateState();
+  });
+
+  passwordConfirmField.addEventListener("field-blur", (e) => {
+    const confirmPassword = e.detail.value;
+    const password = passwordField.value;
+
+    passwordConfirmField.helperText = validateConfirmPassword({
+      password,
+      confirmPassword,
+    });
+    __updateState();
+  });
+
+  usernameField.addEventListener("field-blur", (e) => {
+    const username = e.detail.value;
+    usernameField.helperText = validateUsername(username);
+    __updateState();
+  });
+
+  __updateState();
   return signupPage;
 }
