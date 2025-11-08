a84af0e0980c4d9a968631b8918d215108756b44
happy7yong <happy7yong83@gmail.com>
2025-11-09 01:51:48 +0900

    feat : disabled css 추가

--- src/shared/ui/Button/ui/PrimaryButton.js
+++ src/shared/ui/Button/ui/PrimaryButton.js
@@ -1,4 +1,8 @@
-export default function Button({ text = "버튼", onClick = null } = {}) {
+export default function Button({
+  text = "버튼",
+  onClick = null,
+  state = false,
+} = {}) {
   const button = document.createElement("button");
   button.className = "primaryBtn";
   button.textContent = text;
@@ -8,12 +12,19 @@ export default function Button({ text = "버튼", onClick = null } = {}) {
         .primaryBtn{
                 width:100%;
                 padding:12px;
-                background-color:var(--color-primary);
                 color:#FFFFFF;
                 justify-content:center;
+                background-color:var(--color-primary);
                 border-radius:4px;
                 cursor:pointer;
-        }`;
+        }
+
+        button.disabled,button:disabled{
+          background-color:var(--color-disable);
+          cursor:default;
+        }
+
+        `;
   const handleClick =
     onClick != null ? onClick : () => console.log("버튼을 클릭하였습니다.");
 
