7c1a0c4db2d9e8936e611fd993e633fbeed96e01
happy7yong <happy7yong83@gmail.com>
2025-11-09 03:10:21 +0900

    feat : sessionUser 객체 함수 생성

---
+++ src/shared/utils/session.js
@@ -0,0 +1,20 @@
+const sessionUser = {
+  getUser() {
+    const user = sessionStorage.getItem("user");
+    return user ? JSON.stringify(user) : null;
+  },
+
+  //function 꼭 추가해야하는가?
+  setUser(user) {
+    sessionStorage.setItem("user", JSON.stringify(user));
+  },
+
+  logout() {
+    sessionStorage.removeItem("user");
+  },
+  isLogout() {
+    return !this.getUser();
+  },
+};
+
+export default sessionUser;
