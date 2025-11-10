4a97474dda1d8b37a3f9f20e2d42e8901b642cac
happy7yong <happy7yong83@gmail.com>
2025-11-10 16:42:52 +0900

    feat : post creat, detail page 생성

---
+++ src/pages/board/create/ui/BoardPostCreatPage.js
@@ -0,0 +1,37 @@
+import Title from "../../../../shared/ui/Title/Title.js";
+import textInputField from "../../../../shared/ui/InputField/TextInputField.js";
+
+function BoardPostCreatPage() {
+  const boardPostCreatPage = document.createElement("div");
+  boardPostCreatPage.className = "board-post-creat-page";
+
+  //title
+  const pageTitle = Title({
+    text: "페이지 생성",
+    styleProps: {
+      fontWeight: "var(--font-weight-bold)",
+    },
+  });
+
+  const titleField = textInputField({
+    text: "제목",
+    type: "title",
+    placeholder: "제목을 입력해주세요. (최대 26글자)",
+    helperText: "",
+  });
+
+  const contentField = textInputField({
+    text: "내용",
+    type: "content",
+    placeholder: "내용을 입력해주세요.",
+    helperText: "",
+  });
+
+  boardPostCreatPage.appendChild(pageTitle);
+  boardPostCreatPage.appendChild(titleField);
+  boardPostCreatPage.appendChild(contentField);
+
+  return boardPostCreatPage;
+}
+
+export default BoardPostCreatPage;
---
+++ src/pages/board/detail/ui/BoardPostDetailPage.js
@@ -0,0 +1,43 @@
+import postContent from "./postContent.js";
+import postHeader from "./postHeader.js";
+import postCountGroup from "./postCountGroup.js";
+import commentCount from "../../../../widgets/comment/ui/commentCard.js";
+import commentCreatCard from "../../../../widgets/comment/ui/commentCreatCard.js";
+
+function BoardPostDetailPage() {
+  const boardPostDetailPage = document.createElement("div");
+  boardPostDetailPage.className = "board-post-detail-page";
+
+  //titleHeader
+  const header = postHeader({ title: "안녕", author: "dd", date: "ddd" });
+
+  //content:내용
+  const content = postContent({
+    content:
+      "sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssș",
+  });
+  //countGroup
+  const countGroup = postCountGroup({
+    likeCount: "1",
+    viewCount: "2",
+    commentCount: "3",
+  });
+
+  boardPostDetailPage.appendChild(header);
+  boardPostDetailPage.appendChild(document.createElement("hr"));
+  boardPostDetailPage.appendChild(content);
+  boardPostDetailPage.appendChild(countGroup);
+  boardPostDetailPage.appendChild(document.createElement("hr"));
+
+  //댓글 작성 컴포넌트
+  const contentCreatCard = commentCreatCard();
+  boardPostDetailPage.appendChild(contentCreatCard);
+
+  ///comment card
+  const comment = commentCount({ author: "ddd", date: "dd", content: "ddd" });
+  boardPostDetailPage.appendChild(comment);
+
+  return boardPostDetailPage;
+}
+
+export default BoardPostDetailPage;
