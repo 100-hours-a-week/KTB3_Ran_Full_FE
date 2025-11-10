e5e2f5d06798f65d8a3594bc7a12ff449aadb23d
happy7yong <happy7yong83@gmail.com>
2025-11-10 16:44:04 +0900

    feat : router 이동 경로 추가

--- src/app/router.js/router.js
+++ src/app/router.js/router.js
@@ -1,9 +1,13 @@
 import LoginPage from "../../pages/login/ui/loginPage.js";
 import SignUpPage from "../../pages/signup/ui/SignUpPage.js";
 import BoardHomePage from "../../pages/board/home/ui/BoardHomePage.js";
+import BoardPostCreatPage from "../../pages/board/create/ui/BoardPostCreatPage.js";
+import BoardPostDetailPage from "../../pages/board/detail/ui/BoardPostDetailPage.js";
 
 export const routerPage = {
   "/login": LoginPage,
   "/signup": SignUpPage,
   "/home": BoardHomePage,
+  "/post": BoardPostCreatPage,
+  "/get": BoardPostDetailPage,
 };
