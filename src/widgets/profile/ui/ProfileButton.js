cc06bfeea20b5a1dc6d74faa67e8e75d9c72d27c
happy7yong <happy7yong83@gmail.com>
2025-11-12 20:23:03 +0900

    feat : 프로필 버튼 컴포넌트 생성

---
+++ src/widgets/profile/ui/ProfileButton.js
@@ -0,0 +1,31 @@
+import ProfileMenuModal from "./ProfileMenuModal.js";
+
+export default function ProefileButton() {
+  const profile = document.createElement("div");
+  profile.className = "profile-button";
+  const style = document.createElement("style");
+  style.textContent = /*CSS*/ `
+        .profile-button{
+            display:flex;
+            width:var(--header-side-width);
+            height:40px;
+            border-radius:50%;
+            overflow:hidden;
+            cursor:pointer;
+            background-color:var(--color-secondary);
+        }
+    `;
+
+  const modal = ProfileMenuModal();
+  profile.addEventListener("click", () => {
+    modal.classList.toggle("active");
+  });
+
+  const wrapper = document.createElement("div");
+  wrapper.className = "profile-wrapper";
+  wrapper.appendChild(profile);
+  wrapper.appendChild(modal);
+  wrapper.appendChild(style);
+
+  return wrapper;
+}
