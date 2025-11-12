6a6e126f0fce02015745aa420a30f871d1c4fc3b
happy7yong <happy7yong83@gmail.com>
2025-11-12 21:22:58 +0900

    feat : user상세 페이지 컴포넌트 생성

--- src/pages/info/ui/UserInfoPage.js
+++ src/pages/info/ui/UserInfoPage.js
@@ -1,12 +1,61 @@
+import { updateBtn } from "../../../shared/ui/Button/ui/ButtonPresets.js";
+import Title from "../../../shared/ui/Title/Title.js";
+import inputFieldUser from "../../../widgets/inputField/ui/inputFieldUser.js";
+import DeleteUserButton from "../../../widgets/profile/ui/DeleteUserButton.js";
+
 function UserInfoPage() {
   const container = document.createElement("div");
-  container.className = "";
+  container.className = "user-info-page";
 
-  container.innerHTML = /*HTML*/ `
-  `;
+  const pageTitle = Title({
+    text: "회원정보수정",
+    styleProps: {
+      fontWeight: "var(--font-weight-bold)",
+    },
+  });
+
+  const textInfo = document.createElement("div");
+  textInfo.className = "text-info-container";
+  const inputFieldEmail = inputFieldUser({ title: "이메일", userData: "dd" });
+  const inputFieldUsername = inputFieldUser({
+    title: "닉네임",
+    userData: "dd",
+  });
+  const deleteUserBtn = DeleteUserButton();
+
+  const Btn = updateBtn();
+
+  container.appendChild(pageTitle);
+  textInfo.appendChild(inputFieldEmail);
+  textInfo.appendChild(inputFieldUsername);
+  container.appendChild(textInfo);
+  container.appendChild(Btn);
+  container.appendChild(deleteUserBtn);
 
   const style = document.createElement("style");
   style.textContent = /*CSS*/ `
+    .user-info-page{
+        display: flex;
+            flex-direction: column;
+            justify-content: center;
+            margin: 0 auto;
+            height: 100vh;
+            background-color: var(--color-background);
+            max-width: var(--auth-max-width);
+            gap: var(--gap-md);
+    }
+        .input-field-container{
+            display: flex;
+            gap: 10px;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+
+        .text-info-container{
+            gap: 20px;
+            display: flex;
+            flex-direction: column;
+        }
   `;
 
   const containerWrapper = document.createElement("div");
