cc4bf723dedee84653f1e93c9c26b7ad5c6a4158
happy7yong <happy7yong83@gmail.com>
2025-11-12 14:17:52 +0900

    style : 모달 뒤에 오버레이 추가

---
+++ src/shared/ui/modal/Modal.js
@@ -0,0 +1,84 @@
+import { confirmBtn, quitBtn } from "../Button/ui/ButtonPresets.js";
+
+function Modal({ title = "제목", subTitle = "내용" } = {}) {
+  const container = document.createElement("div");
+  container.className = "modal-wrapper";
+
+  container.innerHTML = /*HTML*/ `
+    <div>${title}</div>
+    <div>${subTitle}</div>
+    <div class = "buttonGroup">
+    </div>
+  `;
+
+  console.log("모달을 클릭하였습니다.");
+
+  const style = document.createElement("style");
+  style.textContent = /*CSS*/ `
+  .modal-wrapper{
+    top: 50%;
+    left: 50%;
+    max-width: var(--max-width-md);
+    transform: translate(-50%, -50%);
+    background: var(--color-card);
+    position:absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 40px 30px;
+    border-radius: 20px;
+    gap: var(--gap-mn);
+    z-index: 2;
+  }
+
+  .modal-wrapper div{
+    font-size: var(--font-size-base);
+  }
+
+  .modal-wrapper div:first-child{
+    font-weight: var(--font-weight-bold);
+    font-size: var(--font-size-lg);
+
+  }
+  .buttonGroup {
+    display:flex;
+    gap:var(--gap-mn);
+  }
+
+  .modal-overlay{
+      position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+  }
+  `;
+
+  //뒤에 오버레이 레이어
+  const overlay = document.createElement("div");
+  overlay.className = "modal-overlay";
+
+  //버튼 생성
+  const confirmButton = confirmBtn();
+  const containerWrapper = document.createElement("div");
+
+  //나가기 버튼
+  const quitButton = quitBtn(containerWrapper);
+
+  const buttonGroup = container.querySelector(".buttonGroup");
+
+  buttonGroup.appendChild(quitButton); //취소 버튼
+  buttonGroup.appendChild(confirmButton); //확인 버튼
+
+  containerWrapper.appendChild(style);
+  containerWrapper.appendChild(container);
+  containerWrapper.appendChild(overlay);
+
+  return containerWrapper;
+}
+
+export default Modal;
+
+//modal 자체는 ui고 handler가
