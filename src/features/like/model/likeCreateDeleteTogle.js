5641a72ff7c05c175c8fbd75dddef564129b3d99
happy7yong <happy7yong83@gmail.com>
2025-11-28 21:27:19 +0900

    feat : likeTogle 일시 저장

--- src/features/like/model/likeCreateDeleteTogle.js
+++ src/features/like/model/likeCreateDeleteTogle.js
@@ -1,20 +1,55 @@
 import handleLikeCreat from "../../../pages/like/lib/handleLikeCreat.js";
 import handleLikeDelete from "../../../pages/like/lib/handleLikeDelete.js";
+import { getState } from "../../../shared/state/currentState.js";
 
-// function likeCreateDeleteTogle({ container, props }) {
-//   if (props.liked) {
-//     container.classList.add("enabled");
-//     container.addEventListener("click", () => handleLikeDelete(props.postId));
-//   } else {
-//     container.classList.remove("enabled");
-//     container.addEventListener("click", () => handleLikeCreat(props.postId));
-//   }
-// }
+function likeCreateDeleteTogle() {
+  //1. DOM 요소 찾기
 
-function likeCreateDeleteTogle(postId) {
-  const state = getState();
+  const like = document.getElementById("likeCount");
+  if (!like) return;
 
-  const updatedPosts = state.posts.map((post) => post.id === postId);
+  const likeImg = like.querySelector(".like-icon");
+  if (!likeImg) return;
+
+  console.log("likeTogle");
+
+  //아이콘 렌더링 함수
+  function updateLikeIcon() {
+    const state = getState();
+    const post = state.post; // 상세 페이지니까 state.post 사용
+
+    if (!post) return; //post 없으면 가라
+
+    likeImg.src = post.liked
+      ? "public/icon/liked_icon.svg"
+      : "public/icon/unliked_icon.svg";
+  }
+
+  async function onClick() {
+    const postId = Number(like.dataset.postId);
+    const post = getState().post;
+
+    if (!post) return;
+
+    if (post.liked) {
+      await handleLikeDelete(postId);
+    } else {
+      await handleLikeCreat(postId);
+    }
+
+    //클릭했으니상태가 바뀜
+    updateLikeIcon();
+  }
+
+  //mount시 초기 렌더링
+  updateLikeIcon();
+
+  like.addEventListener("click", onClick);
+
+  //클린업
+  return () => {
+    like.removeEventListener("click", onClick);
+  };
 }
 
 export default likeCreateDeleteTogle;
