933132ba091b5a344496a9982a989513eeff4f0e
happy7yong <happy7yong83@gmail.com>
2025-11-28 10:23:46 +0900

    feat : 댓글이 생성될 시 새로 고침

--- src/pages/board/detail/lib/handleCreatComment.js
+++ src/pages/board/detail/lib/handleCreatComment.js
@@ -1,6 +1,9 @@
 import commentCreatFetch from "../../../../features/comment/api/commentCreatFetch.js";
+import BoardPostDetailEffect from "../model/BoardPostDetailEffect.js";
+import handlePostDetail from "../../../../features/board/model/handlePostDetail.js";
+import setState from "../../../../shared/state/currentState.js";
 
-function handleCreatComment({ dto, postId }) {
+async function handleCreatComment({ dto, postId }) {
   //댓글 작성 버튼을 누르면 -> 버튼의 input 값을 props에 넣음 -> dto로 동봉하여 보냄
   //필요한 props : postId
   console.log(dto.content);
@@ -9,7 +12,17 @@ function handleCreatComment({ dto, postId }) {
     return;
   }
   console.log(postId);
-  return commentCreatFetch({ dto, postId });
+
+  //comment creat fetch
+  await commentCreatFetch({ dto, postId });
+
+  //comment get fetch : state 변경 및 저장
+  handlePostDetail(postId).then((data) => {
+    setState({
+      post: data.postData,
+      comments: data.commentsData,
+    });
+  });
 }
 
 export default handleCreatComment;
