3dd5de29bae484db8b16a031c364b7981e2ed725
happy7yong <happy7yong83@gmail.com>
2025-11-30 13:02:36 +0900

    docs : 사용하지 않는 코드 주석

--- src/app/LayoutVDOM.js
+++ src/app/LayoutVDOM.js
@@ -4,7 +4,7 @@ import HeaderVDOM from "../widgets/header/ui/HeaderVDOM.js";
 //children으로 가져와서 header를 위에 둔 템플릿
 function LayoutVDOM({ headerMode, children }) {
   return h("div", { className: "container" }, [
-    HeaderVDOM({ mode: headerMode }), //이거 아직 구현 안됨.
+    HeaderVDOM({ mode: headerMode }),
     h("div", { className: "container" }, children),
   ]);
 }
