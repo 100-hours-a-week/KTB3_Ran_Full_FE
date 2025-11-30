d7db95ed5863ccc92d46beeb241fe7ff1584d924
happy7yong <happy7yong83@gmail.com>
2025-11-30 12:01:07 +0900

    feat : post Fetch -> apiFetch 적용

--- src/features/board/api/postDetailFetch.js
+++ src/features/board/api/postDetailFetch.js
@@ -5,32 +5,6 @@ import apiFetch from "../../../shared/api/fetchWrapper.js";
 
 async function postDetail(postId) {
   return apiFetch(`${Endpoint.POST.GET}/${postId}`);
-
-  // const token = sessionStorage.getItem("accessToken");
-  // try {
-  //   const response = await fetch(`${Endpoint.POST.GET}/${postId}`, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //     credentials: "include",
-  //   });
-  //   const json = await response.json();
-  //   console.log(json);
-  //   if (response.ok) {
-  //     const data = json.data;
-  //     const postData = boardDetailProps(data);
-  //     console.log(postData);
-  //     const commentsData = data.comments.map(commentDto);
-  //     return { postData, commentsData };
-  //   } else {
-  //     location.hash = "/";
-  //     throw new Error("error");
-  //   }
-  // } catch (error) {
-  //   throw error;
-  // }
 }
 
 export default postDetail;
