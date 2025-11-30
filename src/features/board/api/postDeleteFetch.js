1590b80e0423c9b2c2573be70bba189c4328284c
happy7yong <happy7yong83@gmail.com>
2025-11-30 12:00:28 +0900

    feat : post Delete apiFetch 적용

--- src/features/board/api/postDeleteFetch.js
+++ src/features/board/api/postDeleteFetch.js
@@ -1,33 +1,11 @@
 import { Endpoint } from "../../../shared/api/endpoint.js";
+import apiFetch from "../../../shared/api/fetchWrapper.js";
 import sessionUser from "../../../shared/utils/session.js";
 
 async function postDeleteFetch(postId) {
-  const token = sessionStorage.getItem("accessToken");
-  try {
-    const response = await fetch(Endpoint.POST.DELETE(postId), {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      credentials: "include",
-    });
-
-    const json = await response.json();
-
-    if (response.ok) {
-      const data = json.data;
-      console.log("data", data);
-      location.hash = "/home";
-
-      return response;
-    } else {
-      console.log("data가 없으세요.", data);
-      throw new Error("error");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  return await apiFetch(Endpoint.POST.DELETE(postId), {
+    method: "DELETE",
+  });
 }
 
 export default postDeleteFetch;
--- src/shared/lib/handlePostDelete.js
+++ src/shared/lib/handlePostDelete.js
@@ -1,11 +1,15 @@
 import postDeleteFetch from "../../features/board/api/postDeleteFetch.js";
 import toast from "../utils/handleToast.js";
 
-function handlePostDelete(postId) {
-  const res = postDeleteFetch(postId);
-  if (res) toast("게시글 삭제");
-
-  return res;
+async function handlePostDelete(postId) {
+  try {
+    await postDeleteFetch(postId);
+    toast("게시글 삭제");
+    location.hash = "/home";
+  } catch (error) {
+    console.error("게시글 삭제 오류:", error);
+    toast("삭제 실패");
+  }
 }
 
 export default handlePostDelete;
