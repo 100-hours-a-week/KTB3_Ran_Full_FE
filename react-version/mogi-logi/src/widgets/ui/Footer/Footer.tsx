1dc5fa8f9b519a2ce55a1959e2faa835a06c2eb9
happy7yong <happy7yong83@gmail.com>
2025-12-02 08:49:23 +0900

    feat : Footer 생성

---
+++ react-version/mogi-logi/src/widgets/ui/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import { FooterLogo } from "../../../shared/ui/logo/FooterLogo";
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-inner">
+        <FooterLogo width={200} />
+
+        <div className="footer-info">
+          <div className="footer-meta">
+            <div className="copyright">
+              © 2024 Chaeyoung Yong. <br />
+              All rights reserved.
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
