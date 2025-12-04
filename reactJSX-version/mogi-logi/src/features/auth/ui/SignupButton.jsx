1525ffdc07f85bcd902aa39cae2300052cd66ae7
happy7yong <happy7yong83@gmail.com>
2025-12-04 16:49:58 +0900

    fix : onClick 불필요 제거

--- reactJSX-version/mogi-logi/src/features/auth/ui/SignupButton.jsx
+++ reactJSX-version/mogi-logi/src/features/auth/ui/SignupButton.jsx
@@ -1,13 +1,8 @@
 import { PrimaryButton } from "../../../shared/ui/button/PrimaryButton";
 
 export function SignupButton(props) {
-  const signup = () => {
-    console.log("회원가입 버튼 클릭");
-  };
-
   const SignupButtonProps = {
     ...props,
-    onClick: signup,
   };
   return <PrimaryButton {...SignupButtonProps} />;
 }
