79c7dc15dfc815e2c50aa660c7c9ec04aef21b50
happy7yong <happy7yong83@gmail.com>
2025-12-04 16:49:07 +0900

    fix : requestApi 오타 변경

--- reactJSX-version/mogi-logi/src/features/auth/hooks/useSignup.js
+++ reactJSX-version/mogi-logi/src/features/auth/hooks/useSignup.js
@@ -3,13 +3,13 @@ import { useApi } from "../../../shared/api/base/useApi";
 import { signupDto } from "../model/authDto";
 
 export function useSignup() {
-  const { responseApi } = useApi();
+  const { requestApi } = useApi();
 
   const handleSignup = async (data) => {
     try {
       //1.dto 가공
       const dto = signupDto(data);
-      const res = await responseApi(Endpoint.USER.SIGNUP, "POST", dto);
+      const res = await requestApi(Endpoint.USER.SIGNUP, "POST", dto);
 
       return res;
     } catch (e) {
