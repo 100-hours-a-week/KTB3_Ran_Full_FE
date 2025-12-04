82044e9397ed8f8292b4fb3f5912600d17ea9d49
happy7yong <happy7yong83@gmail.com>
2025-12-04 16:32:34 +0900

    feat : dto 생성

--- reactJSX-version/mogi-logi/src/features/auth/model/authDto.js
+++ reactJSX-version/mogi-logi/src/features/auth/model/authDto.js
@@ -1,1 +1,11 @@
-const authDto = () => ({});
+export const loginDto = (props) => ({
+  email: props.email,
+  password: props.password,
+});
+
+export const signupDto = (data) => ({
+  email: data.email,
+  password: data.password,
+  confirmPassword: data.confirmPassword,
+  username: data.username,
+});
