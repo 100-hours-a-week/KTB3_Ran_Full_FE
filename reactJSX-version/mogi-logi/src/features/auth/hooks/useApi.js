04726ca0e20655057d514b455f5ab8dfc3b5d00f
happy7yong <happy7yong83@gmail.com>
2025-12-04 16:02:00 +0900

    feat : api base 생성

---
+++ reactJSX-version/mogi-logi/src/features/auth/hooks/useApi.js
@@ -0,0 +1,23 @@
+//인증 실패 공통 처리 custom hook
+//앞으로 얘가 apiFetch를 사용할 겁니다.
+
+import { useNavigate } from "react-router-dom";
+import apiFetch from "../../../shared/api/base/apiFetch";
+
+export function useApi() {
+  const navigator = useNavigate();
+  const requestApi = async (url, method, body, options) => {
+    const res = await apiFetch(url, method, body, options);
+
+    //실패 처리
+    if (!res.ok) {
+      sessionStorage.clear();
+      navigator("/login");
+      return console.log("로그인페이지로 이동");
+    }
+
+    const requestData = res.data;
+    return requestData;
+  };
+  return { requestApi };
+}
