cc6f25a1ac57f8149658ec292f5484752c87264e
happy7yong <happy7yong83@gmail.com>
2025-12-04 16:15:16 +0900

    feat : 유효성 검사 : 비밀번호 확인, 닉네임 추가

--- reactJSX-version/mogi-logi/src/features/auth/lib/validater.jsx
+++ reactJSX-version/mogi-logi/src/features/auth/lib/validater.jsx
@@ -21,3 +21,21 @@ export function validatePassword(password) {
 
   return "";
 }
+
+export function validateUsername(username) {
+  // 띄어쓰기 불가 10글자 이내
+  // 비어있을 시
+  // 중복된 닉네임
+  if (!username) return "*닉네임을 입력하세요.";
+  if (username.includes(" ")) return "*띄어쓰기를 없애주세요.";
+  if (username.length > 10)
+    return "*닉네임은 최대 10글자 까지 작성 가능합니다.";
+  return "";
+}
+
+export function validateConfirmPassword({ password, confirmPassword }) {
+  if (!confirmPassword) return "*비밀번호를 한번 더 입력해주세요.";
+  if (password != confirmPassword) return "*비밀번호가 다릅니다.";
+
+  return "";
+}
