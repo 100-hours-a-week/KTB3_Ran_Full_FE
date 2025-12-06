6361de6ad36641f50b8d32c8f74cbd96c657bc5d
happy7yong <happy7yong83@gmail.com>
2025-12-06 11:18:31 +0900

    style : 상세 페이지 스타일 변경

--- reactJSX-version/mogi-logi/src/features/actionGroup/ui/ActionGroup.jsx
+++ reactJSX-version/mogi-logi/src/features/actionGroup/ui/ActionGroup.jsx
@@ -1,15 +1,16 @@
 import "../style/actionGroup.css";
+import { Icon } from "../../../shared/ui/icons/Icon.jsx";
 
 export function ActionGroup({ onEdit, onDelete }) {
   return (
     <div className="action-group-wrapper">
       <div className="action-group">
         <button className="action-btn" onClick={onEdit}>
-          수정
+          <Icon name={"edit"} size={25} />
         </button>
 
         <button className="action-btn danger" onClick={onDelete}>
-          삭제
+          <Icon name={"delete"} style={{ color: "red" }} size={25} />
         </button>
       </div>
     </div>
--- reactJSX-version/mogi-logi/src/features/comment/update/ui/CommentUpdateButton.jsx
+++ reactJSX-version/mogi-logi/src/features/comment/update/ui/CommentUpdateButton.jsx
@@ -1,8 +1,8 @@
-import { SecondaryButton } from "../../../../shared/ui/button/SecondaryButton.jsx";
+import { SecondaryOutlineButton } from "../../../../shared/ui/button/SecondaryButton.jsx";
 
 export function CommentUpdateButton(props) {
   const CommentUpdateProps = {
     ...props,
   };
-  return <SecondaryButton {...CommentUpdateProps} />;
+  return <SecondaryOutlineButton {...CommentUpdateProps} />;
 }
