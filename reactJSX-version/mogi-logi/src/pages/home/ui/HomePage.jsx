10d0f6617ccd260303c993572f793ff246fc8271
happy7yong <happy7yong83@gmail.com>
2025-12-07 14:11:08 +0900

    feat : 새로고침 시 스크롤 복원 -> 훅으로 분리

--- reactJSX-version/mogi-logi/src/pages/home/ui/HomePage.jsx
+++ reactJSX-version/mogi-logi/src/pages/home/ui/HomePage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 import { PostCard } from "../../../entities/post/ui/PostCard";
 import { PostCreateNavButton } from "../../../features/post/create/ui/PostNavIconButton";
 import { useHomeInfiniteQueue } from "../../../features/home/hooks/useHomeInfiniteQueue.js";
+import { useScrollStore } from "../../../shared/lib/useScrollStore.js";
 
 export function HomePage() {
   const loaderRef = useRef(null);
@@ -10,48 +11,45 @@ export function HomePage() {
   const { data, fetchNextPage, hasNextPage, isLoading } =
     useHomeInfiniteQueue();
 
-  //1) 스크롤 위치 저장
+  const { saveScroll, savePageIndex, getSavedState } = useScrollStore();
+
+  //1. 스크롤 위치 저장
   useEffect(() => {
-    const saveScroll = () => {
-      sessionStorage.setItem("home_scrollY", window.scrollY);
-    };
-    window.addEventListener("scroll", saveScroll);
-    return () => window.removeEventListener("scroll", saveScroll);
+    const save = () => saveScroll();
+    window.addEventListener("scroll", save);
+    return () => window.removeEventListener("scroll", save);
   }, []);
 
   //2) 새로고침 시 스크롤 + 페이지 복원
   useEffect(() => {
-    const savedScrollY = Number(sessionStorage.getItem("home_scrollY")) || 0;
-    const savedPage = Number(sessionStorage.getItem("home_page_index")) || 0;
-
-    if (savedScrollY === 0) return; // 저장 기록 없으면 패스
+    const { scrollY, pageIndex } = getSavedState();
+    if (scrollY === 0 && pageIndex === 0) return; // 저장 기록 없으면 패스
 
     let pageLoaded = 0;
 
     // 저장된 페이지까지 fetch 반복
-    const restorePages = async () => {
-      while (pageLoaded < savedPage && hasNextPage) {
+    const restore = async () => {
+      while (pageLoaded < pageIndex && hasNextPage) {
         await fetchNextPage();
         pageLoaded++;
       }
 
       // 페인트 이후 스크롤 복원
       requestAnimationFrame(() => {
-        window.scrollTo(0, savedScrollY);
+        window.scrollTo(0, scrollY);
       });
     };
 
-    restorePages();
+    restore();
   }, [fetchNextPage, hasNextPage]);
 
   //3) 페이지 로드될 때마다 현재 페이지 index 저장
   useEffect(() => {
     if (!data) return;
-    const currentPageIndex = data.pages.length - 1;
-    sessionStorage.setItem("home_page_index", currentPageIndex);
+    savePageIndex(data.pages.length - 1);
   }, [data]);
 
-  // 4) Intersection Observer
+  // 4) 옵저버
 
   useEffect(() => {
     if (!loaderRef.current) return;
